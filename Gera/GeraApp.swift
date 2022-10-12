fb19f673d8a5609a5ecc662fe9ad36d8ccfc73ab
Matheus Batista <matheus.vbatista04@gmail.com>
2022-10-12 00:44:40 -0300

    first commit

---
+++ Gera/GeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeraApp.swift
+//  Gera
+//
+//  Created by matheusvb on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
