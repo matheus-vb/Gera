f0f3a5475f405553c3899f12353afb7645478925
ditthales <tvgf@academy.cin.ufpe.br>
2022-10-27 14:48:50 -0300

    fixing bugs

--- Gera/View/ConnexionScreenView.swift
+++ Gera/View/ConnexionScreenView.swift
@@ -8,51 +8,55 @@
 import SwiftUI
 
 struct ConnexionScreenView: View{
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     var body: some View{
         GeometryReader { geometry in
-            ZStack(alignment: .center){
-                BackgroundConnexionScreen()
-                VStack(alignment: .center){
-                    HStack(){
+            NavigationView{
+                ZStack(alignment: .center){
+                    BackgroundConnexionScreen()
+                    VStack(alignment: .center){
+                        HStack(){
+                            Button(action: {
+                                presentationMode.wrappedValue.dismiss()
+                            }) {
+                                Image("Back_Button")
+                            }
+                            Spacer()
+                            Button(action: {
+                                print("d")
+                            }) {
+                                Image("Sound_Button")
+                            }
+                        }
+                        .padding(.horizontal)
+                        Spacer()
+                        Text("ESCOLHA SEU CIENTISTA PARCEIRO").font(.system(size: 24)).fontWeight(.medium).multilineTextAlignment(.center)
+                        Spacer()
                         Button(action: {
                             print("d")
                         }) {
-                            Image("Back_Button")
+                            MyButton(text: "Criar Sala", icon: "Add_Button", isBig: true)
                         }
                         Spacer()
                         Button(action: {
                             print("d")
                         }) {
-                            Image("Sound_Button")
+                            MyButton(text: "Selecionar Parceiro", icon: "Find_Button", isBig: true)
                         }
+                        Spacer()
+                        Button(action: {
+                            print("d")
+                        }) {
+                            MyButton(text: "Ranking", icon: "Rank_Button", isBig: false)
+                        }
+                        Spacer()
                     }
-                    .padding(.horizontal)
-                    Spacer()
-                    Text("ESCOLHA SEU CIENTISTA PARCEIRO").font(.system(size: 24)).fontWeight(.medium).multilineTextAlignment(.center)
-                    Spacer()
-                    Button(action: {
-                        print("d")
-                    }) {
-                        MyButton(text: "Criar Sala", icon: "Add_Button", isBig: true)
-                    }
-                    Spacer()
-                    Button(action: {
-                        print("d")
-                    }) {
-                        MyButton(text: "Selecionar Parceiro", icon: "Find_Button", isBig: true)
-                    }
-                    Spacer()
-                    Button(action: {
-                        print("d")
-                    }) {
-                        MyButton(text: "Ranking", icon: "Rank_Button", isBig: false)
-                    }
-                    Spacer()
+                    .padding(.vertical)
+                    .frame(width: 306.13, height: geometry.size.height * 0.8)
+                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.5)
                 }
-                .padding(.vertical)
-                .frame(width: 306.13, height: geometry.size.height * 0.87 * 0.88)
-                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.54)
-            }
+            }.navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.vertical)
         }.edgesIgnoringSafeArea(.vertical)
     }
 }
